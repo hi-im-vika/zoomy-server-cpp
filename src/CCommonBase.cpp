cfdb082188d558041d640fcf6b385eba423bef79
vika <37137412+hi-im-vika@users.noreply.github.com>
2024-04-16 14:14:21 -0700

    Removed unnecessary performance counter code

--- src/CCommonBase.cpp
+++ src/CCommonBase.cpp
@@ -9,21 +9,14 @@
 CCommonBase::~CCommonBase() = default;
 
 void CCommonBase::run() {
+    // start update thread
     std::thread thread_for_updating(update_thread, this);
     thread_for_updating.detach();
     do {
-        // we started at this time
-        _perf_draw_start = std::chrono::steady_clock::now();
         // do draw
         draw();
-        // update elapsed time since draw
-        int since_start = (int) std::chrono::duration_cast<std::chrono::milliseconds>(
-                std::chrono::steady_clock::now() - _perf_draw_start).count();
-        // announce elapsed time from draw alone
-        spdlog::debug("Completed in " + std::to_string(since_start) + " ms");
-        // after blocking complete, update total elapsed time
-        _perf_draw = since_start;
     } while (!_do_exit);
+    // handle exit
     if (thread_for_updating.joinable()) thread_for_updating.join();
 }
 
