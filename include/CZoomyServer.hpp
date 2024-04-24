7f04fd39f3b1d966686bd518ab1c9483394aa664
vika <37137412+hi-im-vika@users.noreply.github.com>
2024-04-24 11:40:42 -0700

    Add base working server code (echo client data back)

--- include/CZoomyServer.hpp
+++ include/CZoomyServer.hpp
@@ -12,10 +12,28 @@
 
 class CZoomyServer : public CCommonBase {
 private:
+    // net
+    std::string _port;
+    sockaddr_in _client{};
+    CUDPServer _server;
+    std::thread _thread_tx, _thread_rx;
+    std::chrono::steady_clock::time_point _timeout_count;
+    int _time_since_start;
+    std::queue<std::string> _tx_queue, _rx_queue;
+
+    std::vector<uint8_t> _rx_buf;
+    long _rx_bytes;
+
+    void rx();
+    void tx();
+
 public:
-    CZoomyServer();
+    CZoomyServer(std::string port);
     ~CZoomyServer();
 
     void update() override;
     void draw() override;
+
+    static void thread_rx(CZoomyServer *who_called);
+    static void thread_tx(CZoomyServer *who_called);
 };
--- src/CZoomyServer.cpp
+++ src/CZoomyServer.cpp
@@ -6,23 +6,86 @@
 
 #include "../include/CZoomyServer.hpp"
 
-CZoomyServer::CZoomyServer() = default;
+#define PING_TIMEOUT 1000
+#define NET_DELAY 1
+
+CZoomyServer::CZoomyServer(std::string port) {
+    _port = port;
+
+    // net init
+    _timeout_count = std::chrono::steady_clock::now();
+    _time_since_start = 0;
+
+    _server.setup(_port);
+
+    // start listen thread
+    _thread_rx = std::thread(thread_rx, this);
+    _thread_rx.detach();
+
+    // start send thread
+    _thread_tx = std::thread(thread_tx, this);
+    _thread_tx.detach();
+}
 
 CZoomyServer::~CZoomyServer() = default;
 
 void CZoomyServer::update() {
-    spdlog::info("Server update");
-    std::this_thread::sleep_until(std::chrono::system_clock::now() + std::chrono::milliseconds(10));
+//    spdlog::info("Server update");
+
+    for (; !_rx_queue.empty(); _rx_queue.pop()) {
+
+//            // acknowledge next data in queue
+//            spdlog::info("New in RX queue: " + rx_queue.front());
+//            spdlog::info("Remaining in queue: " + std::to_string(rx_queue.size()));
+
+        // echo client data back to client
+        _tx_queue.emplace(_rx_queue.front());
+    }
+
+    _time_since_start = (int) std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - _timeout_count).count();
+    std::this_thread::sleep_until(std::chrono::system_clock::now() + std::chrono::milliseconds(NET_DELAY));
 }
 
 void CZoomyServer::draw() {
-    spdlog::info("Server draw");
+//    spdlog::info("Server draw");
+    // servo control code goes here...
     std::this_thread::sleep_until(std::chrono::system_clock::now() + std::chrono::milliseconds(10));
+}
+
+void CZoomyServer::rx() {
+    _rx_bytes = 0;
+    _rx_buf.clear();
+    _server.do_rx(_rx_buf, _client, _rx_bytes);
+    std::string temp = std::string(_rx_buf.begin(),_rx_buf.begin() + _rx_bytes);
+    // only add to rx queue if data is not empty
+    if(!temp.empty()) _rx_queue.emplace(temp);
+    std::this_thread::sleep_until(std::chrono::system_clock::now() + std::chrono::milliseconds(NET_DELAY));
+}
+
+void CZoomyServer::tx() {
+    for (; !_tx_queue.empty(); _tx_queue.pop()) {
+//            spdlog::info("Sending");
+        std::vector<uint8_t> tx_buf(_tx_queue.front().begin(),_tx_queue.front().end());
+        _server.do_tx(tx_buf,_client);
+    }
+    std::this_thread::sleep_until(std::chrono::system_clock::now() + std::chrono::milliseconds(NET_DELAY));
+}
+
+void CZoomyServer::thread_rx(CZoomyServer *who_called) {
+    while (!who_called->_do_exit) {
+        who_called->rx();
+    }
+}
+
+void CZoomyServer::thread_tx(CZoomyServer *who_called) {
+    while (!who_called->_do_exit) {
+        who_called->tx();
+    }
 }
 
 int main() {
 //    std::cout << "Hello, World!" << std::endl;
-    CZoomyServer c = CZoomyServer();
+    CZoomyServer c = CZoomyServer("46188");
     c.run();
     return 0;
 }
