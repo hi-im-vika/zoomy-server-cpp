95a174cb85573a3b0d815ae1424d180df332aa68
vika <37137412+hi-im-vika@users.noreply.github.com>
2024-04-29 14:14:02 -0700

    Begin i2c development

--- include/CControlPi.hpp
+++ include/CControlPi.hpp
@@ -31,38 +31,18 @@ private:
     int _servo_pos_converted;
     uint8_t input_buffer[3];
 public:
-    enum gpio_pins {
-        BUTTON1 = 17,
-        BUTTON2 = 27,
-        LED1 = 22,
-        SERVO1 = 12,
-        HEARTBEAT = 17,
-        STEERING = 12,
-        THROTTLE = 13
-    };
-    enum data_type {
-        ANALOG,
-        DIGITAL,
-        SERVO
-    };
-    enum adc_channel {
-        CH0 = 0x80,
-        CH1 = 0x90,
-    };
-    enum data_op {
-        READ,
-        WRITE
-    };
-    enum servo {
-        PWM_FREQ = 100,
+    enum i2c_ch {
+        CH0,
+        CH1
     };
     CControlPi() = default;
     ~CControlPi() = default;
     void init();
     void init_gpio(const std::vector<int> &input_pins, std::vector<int> &output_pins);
-    bool get_data(data_type type, int channel, int &result);
-    bool set_data(data_type type, int channel, int val);
-    bool servo_talker(CControlPi::data_op op, int channel, int set_val, int &result);
-    bool code_talker(adc_channel ch, int &result);
+    int init_i2c(i2c_ch ch, uint address);
+//    bool get_data(data_type type, int channel, int &result);
+//    bool set_data(data_type type, int channel, int val);
+//    bool servo_talker(CControlPi::data_op op, int channel, int set_val, int &result);
+//    bool code_talker(adc_channel ch, int &result);
     void zap_com();
 };
